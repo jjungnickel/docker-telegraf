977615eb2b102172f4b5fa46f919489df3afe0c1
Jan Jungnickel <jan@jungnickel.com>
2016-01-22 16:33:05 +0100

    No need to wait for InfluxDB

--- run.sh
+++ run.sh
@@ -2,12 +2,5 @@
 
 envtpl /etc/telegraf/telegraf.conf.tpl
 
-echo -n 'Waiting for InfluxDB API'
-while ! (ret=$(curl -I -s "$INFLUXDB_URL/ping" -o /dev/null -w "%{http_code}"); [ $ret -eq 204 ]) do 
-	echo -n '.' 
-	sleep 3s
-done
-echo '\nInfluxDB API Ready'
-
 telegraf -config /etc/telegraf/telegraf.conf
 
