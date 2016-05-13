117c7e83048ee65b3ec94f604faf5a44ee56efdb
Nicolas Degory <ndegory@axway.com>
2016-05-13 09:56:03 +0000

    use exec for main process

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "Configured outputs:"
+echo "InfluxDB:     $OUTPUT_INFLUXDB_ENABLED"
+echo "Cloudwatch:   $OUTPUT_CLOUDWATCH_ENABLED"
+echo "Kafka:        $OUTPUT_KAFKA_ENABLED"
 envtpl /etc/telegraf/telegraf.conf.tpl
 
-/bin/telegraf -config /etc/telegraf/telegraf.conf
+exec /bin/telegraf -config /etc/telegraf/telegraf.conf
 
